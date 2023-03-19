e0ba93689cf5dd1c8f3079ba973a9d23e98eb2a0
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-19 20:14:48 +0800

    Adds seed for Record

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,9 +25,9 @@ curator = User.create(email: 'bob@bob.com', password: '123456', username: 'bob',
 User.create(email: 'carol@carol.com', password: '123456', username: 'carol', role: 'admin')
 
 puts "Creating jukeboxes..."
-Jukebox.create(name: 'best', user: curator)
-Jukebox.create(name: 'better', user: curator)
-Jukebox.create(name: 'good', user: curator)
+best = Jukebox.create(name: 'best', user: curator)
+better = Jukebox.create(name: 'better', user: curator)
+good = Jukebox.create(name: 'good', user: curator)
 
 puts "Creating artists..."
 beatles = Artist.create(name: 'The Beatles')
@@ -46,11 +46,19 @@ rock = Genre.create(name: 'rock')
 country = Genre.create(name: 'country')
 
 puts "Creating tracks..."
-Track.create(title: "Let It Be", album: album1, genre: pop)
-Track.create(title: "Here Comes the Sun", album: album2, genre: pop)
-Track.create(title: "Come Together", album: album2, genre: pop)
-Track.create(title: "Love of My Life", album: album3, genre: rock)
-Track.create(title: "Bohemian Rhapsody", album: album3, genre: rock)
-Track.create(title: "Leaving on a Jet Plane", album: album4, genre: country)
+track1 = Track.create(title: "Let It Be", album: album1, genre: pop)
+track2 = Track.create(title: "Here Comes the Sun", album: album2, genre: pop)
+track3 = Track.create(title: "Come Together", album: album2, genre: pop)
+track4 = Track.create(title: "Love of My Life", album: album3, genre: rock)
+track5 = Track.create(title: "Bohemian Rhapsody", album: album3, genre: rock)
+track6 = Track.create(title: "Leaving on a Jet Plane", album: album4, genre: country)
+
+puts "Creating records..."
+Record.create(jukebox: best, track: track1)
+Record.create(jukebox: best, track: track2)
+Record.create(jukebox: best, track: track4)
+Record.create(jukebox: best, track: track6)
+Record.create(jukebox: better, track: track3)
+Record.create(jukebox: better, track: track5)
 
 puts "Finished"
