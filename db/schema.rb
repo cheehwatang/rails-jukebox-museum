e599305ce0b33ac06cafd4f57190804c80cfd50b
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-19 16:37:18 +0800

    Create Track model and tracks table with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_19_083404) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_19_083631) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_19_083404) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tracks", force: :cascade do |t|
+    t.string "title", null: false
+    t.bigint "genre_id", null: false
+    t.bigint "album_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["album_id"], name: "index_tracks_on_album_id"
+    t.index ["genre_id"], name: "index_tracks_on_genre_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -49,4 +59,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_19_083404) do
   end
 
   add_foreign_key "albums", "artists"
+  add_foreign_key "tracks", "albums"
+  add_foreign_key "tracks", "genres"
 end
