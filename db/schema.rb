f6fd593bb7f9d243ae5fd6b7208c524ee0289843
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-19 16:35:03 +0800

    Create Genre model and genres table with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_19_083006) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_19_083404) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_19_083006) do
   end
 
   create_table "artists", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
