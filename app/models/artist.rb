bf06ba66781ccf2433503d640c3df00cc1bacc8e
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-19 16:41:03 +0800

    Adds validations and associations for Track, Genre, Album and Artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,6 @@
 class Artist < ApplicationRecord
   has_many :albums, dependent: :destroy
+  has_many :tracks, through: :albums
 
   validates :name, presence: true
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,6 @@
 class Track < ApplicationRecord
   belongs_to :genre
   belongs_to :album
+
+  validates :title, presence: true
 end
