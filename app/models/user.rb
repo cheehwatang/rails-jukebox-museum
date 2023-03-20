c3e0e5c7a48fb5eca237e8e26b0e8a2d9f4a6b0d
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-20 20:46:06 +0800

    Adds validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  enum role: %i[user curator admin]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of %i[username role email]
+  validates :email, uniqueness: true
 end
