f392b5c3d5f55c8f224436bfa883b5491143df4f
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-20 21:03:16 +0800

    Adds seed for User

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,8 @@ Jukebox.destroy_all
 
 puts "Destroying users..."
 User.destroy_all
+
+puts "Creating users..."
+User.create!(email: 'alice@alice.com', password: '123456', username: '12136120750', role: 0)
+User.create!(email: 'bob@bob.com', password: '123456', username: '12136120750', role: 1)
+User.create!(email: 'carol@carol.com', password: '123456', username: '12136120750', role: 2)
